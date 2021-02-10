07007f17dbdabfe58c3667af842dba252b1ee0f9
Thomas Buß <tbuss93@gmail.com>
2021-02-10 21:34:48 +0100

    Minor style changes

--- PerimeterOfPolygon.fsx
+++ PerimeterOfPolygon.fsx
@@ -1,19 +1,20 @@
 open System
 
 let n = Console.ReadLine() |> int
-let coords = 
-    stdin.ReadToEnd().Split "\n"
-    |> Array.map (fun str -> str.Trim() )
-    |> Array.filter (fun str -> str <> "")
-    |> Array.map (fun x -> x.Split " ")
-    |> Array.map (fun xs -> (int(xs.[0]), int(xs.[1])))
-    |> Array.toList
 
-let distance (x1: int, y1: int) (x2: int, y2: int) =
+let coords =
+    stdin.ReadToEnd().Split "\n"
+    |> Seq.map (fun str -> str.Trim())
+    |> Seq.filter (fun str -> str <> "")
+    |> Seq.map ( (fun x -> x.Split " ") >> (fun xs -> (int (xs.[0]), int (xs.[1]))))
+    |> Seq.toList
+
+let distance (x1, y1) (x2, y2) =
     Math.Sqrt(float ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
 
 let mutable sum = 0.0
-for i in [0..n-1] do
-    sum <- sum + distance coords.[i] coords.[(i+1)%n]
+
+for i in [ 0 .. n - 1 ] do
+    sum <- sum + distance coords.[i] coords.[(i + 1) % n]
 
 printfn "%f" sum
